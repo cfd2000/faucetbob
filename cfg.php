13f62c08ba6d2adb4910b46fa1933f6179b41aea
cfd2000 <52366440+cfd2000@users.noreply.github.com>
2022-05-10 12:52:02 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,20 @@
+<?php
+
+//masukkan cookie lu (wajib)
+
+$cookie = "PHPSESSID=ebg7nv17smder3re74i5ciu34s;RefSource=https%3A%2F%2Fr.search.yahoo.com%2F_ylt%3DAwrWp2RF.HliiSsADiVEDN04%3B_ylu%3DY29sbwNncTEEcG9zAzEEdnRpZAMEc2VjA3Ny%2FRV%3D2%2FRE%3D1652189381%2FRO%3D10%2FRU%3Dhttps%253a%252f%252ffaucetofbob.xyz%252f%2FRK%3D2%2FRS%3D7wobXR84bB7BthgCdjbpord2_7I-;_ga=GA1.2.1217820801.1652160591;_gid=GA1.2.1292095419.1652160591;SesHashKey=5iuz2lqlvd4i0xpn;SesToken=ses_id%3D39049%26ses_key%3D5iuz2lqlvd4i0xpn;AccExist=39049;a=g4Q4gCDmUW36F6q6i1R3mByUfw0nMS7A;_gat_gtag_UA_145431796_1=1;token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAYnn4fAFiefjvgAGBAsAAIGF3KC7x5ANqnlo9wEuVKnL9s8aAhTnGewZfAH6CT3t4wQBHMEUCIQDXAH0uGBpFpru1feRaz59a1bAuucys633p5GErC-GKvwIgCrkZNb_VT0OJLhimTST8CZ8s7ygPwIefGlRN9umxElk";
+
+//masukkan user agent lu, kalo ga tau ya udah ;-;
+$useragent = "Mozilla/5.0 (Linux; Android 8.1.0; CPH1803) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Mobile Safari/537.36";
+
+/*
+pilih waktu sesuka hati, recomend 60
+2 = 120
+5 = 300
+10 = 600
+contoh: $waktu_pencari = "60";
+*/
+$waktu_pencari = "60";
+
+//link ga usah di ganti -_-
+$link = "https://faucetofbob.xyz/scratch.html";
